2a6a360f4424466e88428a63ce627e99e5d5b125
Hideyuki Utsunomiya <hidechae@gmail.com>
2025-10-31 02:24:11 +0900

    fix lint

--- src/server/api/routers/bingo.ts
+++ src/server/api/routers/bingo.ts
@@ -1,10 +1,6 @@
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import {
   getGridSize,
   getRequiredSongCount,
