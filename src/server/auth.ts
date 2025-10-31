14841a3d1f479b0aefe4a03e96b3ea176c9cc5d8
Hideyuki Utsunomiya <hidechae@gmail.com>
2025-10-31 16:32:10 +0900

    fix: 同じメールアドレスで複数のOAuthプロバイダーを使用可能にする
    - GoogleとSpotifyプロバイダーにallowDangerousEmailAccountLinkingを追加
    - OAuthAccountNotLinkedエラーを解消
    - 同じメールアドレスでMagic Link、Google、Spotifyのいずれでもログイン可能に

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -63,6 +63,7 @@ const createProviders = () => {
       GoogleProvider({
         clientId: env.GOOGLE_CLIENT_ID,
         clientSecret: env.GOOGLE_CLIENT_SECRET,
+        allowDangerousEmailAccountLinking: true, // 同じメールアドレスで複数のプロバイダーを使用可能にする
       })
     );
   } else {
@@ -76,6 +77,7 @@ const createProviders = () => {
       SpotifyProvider({
         clientId: env.SPOTIFY_CLIENT_ID,
         clientSecret: env.SPOTIFY_CLIENT_SECRET,
+        allowDangerousEmailAccountLinking: true, // 同じメールアドレスで複数のプロバイダーを使用可能にする
         authorization: {
           params: {
             scope:
